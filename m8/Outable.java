7011d97970c32ab0c3d04c52c5622fc3c636caee
Левченко Владислав <90248828+mered1t@users.noreply.github.com>
2022-11-26 20:37:41 +0300

    Delete m8 directory

--- m8/Outable.java
+++
@@ -1,3 +0,0 @@
-public interface Outable {
-    void outputClassName();
-}
--- m8/Shape.java
+++
@@ -1,6 +0,0 @@
-public abstract class Shape implements Outable{
-    @Override
-    public void outputClassName() {
-        System.out.println("This is " + this.getClass());
-    }
-}
