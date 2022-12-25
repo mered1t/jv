d4c31d75025a2c884471bb879ff159d23078b4c8
Левченко Владислав <90248828+mered1t@users.noreply.github.com>
2022-12-25 22:56:07 +0200

    Add files via upload

---
+++ module10/module10_2/src/SolutionFinder.java
@@ -0,0 +1,67 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.util.List;
+import java.util.Random;
+
+public class SolutionFinder {
+    public void createFile(String fileName){
+        File file = new File(fileName);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+
+            fileWriter.write("name age\n");
+            fileWriter.write(fillFile());
+            fileWriter.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());}
+    }
+
+    private String fillFile(){
+        StringBuilder text = new StringBuilder();
+        Random rand = new Random();
+
+        for (int i = 0; i < rand.nextInt(20); i++)
+        {
+        text.append(createRandomWord(rand.nextInt(10)) + " " + rand.nextInt(99) + "\n");
+        }
+        return text.toString();
+    }
+
+    private String createRandomWord(int len){
+        String name = "";
+        for (int i = 0; i < len; i++) {
+            int v = 1 + (int) (Math.random() * 26);
+            char c = (char) (v + (i == 0 ? 'A' : 'a') - 1);
+            name += c;
+        }
+        return name;
+    }
+
+    public void readFile(List<User> list, String fileName){
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if(line.equals("name age")){continue;}
+                String[] userData = line.split("\\s+");
+                list.add(new User(userData[0], Integer.parseInt(userData[1])));
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());}
+    }
+
+    public void toJsonList(List<User> list, String fileName){
+        File file = new File(fileName);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String jsonString = gson.toJson(list);
+            fileWriter.write(jsonString);
+
+            fileWriter.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());}
+    }
+}
---
+++ module10/module10__3/untitled/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        SolutionFinder solutionFinder = new SolutionFinder();
+        HashMap<String, Integer> result = new LinkedHashMap();
+        solutionFinder.processMethods(result);
+    }
+}
