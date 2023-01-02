6fda304518748c1d737eb9f745b151aaa2974524
Левченко Владислав <wizardukche@gmail.com>
2023-01-02 22:25:22 +0200

    module10task3

---
+++ module10/module10task3/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        SolutionFinder solutionFinder = new SolutionFinder();
+        HashMap<String, Integer> result = new LinkedHashMap();
+        solutionFinder.processMethods(result);
+    }
+}
