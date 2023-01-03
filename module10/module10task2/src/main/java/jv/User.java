dd8d0591a6222be1fb2a83d5dcc51697af48f1ab
Левченко Владислав <wizardukche@gmail.com>
2023-01-03 19:31:32 +0200

    module10task2

---
+++ module10/module10task2/src/main/java/jv/User.java
@@ -0,0 +1,12 @@
+package jv;
+
+public class User {
+
+    private final String name;
+    private final int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ module10/module10task2/src/test/java/jv/Module10task2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module10task2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
